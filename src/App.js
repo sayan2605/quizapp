8358b59f630ead1920a8b71fd590caa684bab83b
Sayan Roy <synroy1947@gmail.com>
2023-02-17 14:03:56 +0530

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Data } from './Data/Data';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      console.log({Data});
+    </div>
+  );
+}
+
+export default App;
