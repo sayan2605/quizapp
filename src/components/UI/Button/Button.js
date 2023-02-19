b13e3cf34827e28eb61107d4d8eee952184b7186
Debarshi Das <debarshi8das@gmail.com>
2023-02-19 13:30:14 +0530

    disable button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,7 +4,14 @@ import { useState } from "react";
 const Button = ({ option, question, answer }) => {
   const [isCorrect, setIsCorrect] = useState(false);
 
+  const handleButton = () => {
+    const button = document.querySelectorAll("q" + question);
+    button.forEach((button) => {
+      button.classList.add("disabled");
+    });
+
   const handleButtonClick = () => {
+    handleButton();
     if (option === answer) {
       setIsCorrect(true);
     }
