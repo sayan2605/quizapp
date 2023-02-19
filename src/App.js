83de1fd035095bfb54b90fe669c03ca4ab9d2b1d
Debarshi Das <debarshi8das@gmail.com>
2023-02-19 16:17:02 +0530

    f

--- src/App.js
+++ src/App.js
@@ -12,6 +12,25 @@ function App() {
           return <Card key={index} value={value} />;
         })}
       </div>
+      <button
+        className="submitButton noShow"
+        onClick={() => {
+          document.querySelector(".quizApp").classList.add("noShow");
+          document.querySelector(".submitButton").classList.add("noShow");
+
+          const correctAnswers = document.querySelectorAll(".correct");
+          const correctAnswerCount = correctAnswers.length;
+
+          const result = document.querySelector(".quizResult");
+          result.innerHTML = `You have answered ${correctAnswerCount} / 5 questions correctly`;
+
+          <Banner />;
+          document.querySelector(".banner").classList.remove("noShow");
+          document.querySelector(".quizResult").classList.remove("noShow");
+        }}
+      >
+        Show Results
+      </button>
     </div>
   );
 }
